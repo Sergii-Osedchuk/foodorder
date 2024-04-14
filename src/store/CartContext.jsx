e2bd5d70eb433ed0ca740a210451e80515cd4457
Sergio <norgestream@gmail.com>
2024-04-14 23:12:42 +0300

    start adding logic to CartContextProvider

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,12 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {}
 });
+function cartReducer(state, action) {
+  if(action.type === 'ADD_ITEM') {
+    //....update state to add
+  }
+
+  if(action.type === 'REMOVE_ITEM') {
+    //...
+  }
+
+  return state;
+}
 
 export function CartContextProvider({children}) {
+  useReducer();
+
   return <CartContext.Provider>{children}</CartContext.Provider>
 }
 
