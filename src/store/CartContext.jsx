1134ad3e069e2b28df0a2c682b88e2a1e91bbdd7
Sergio <norgestream@gmail.com>
2024-04-15 16:02:26 +0300

    add ability to add items to cart in reducer function

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -7,7 +7,24 @@ const CartContext = createContext({
 });
 function cartReducer(state, action) {
   if(action.type === 'ADD_ITEM') {
-    //....update state to add
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+
+    } else {
+      updatedItems.push({...action.item, quantity: 1});
+    }
+
+    return {...state, items: updatedItems};
   }
 
   if(action.type === 'REMOVE_ITEM') {
@@ -18,7 +35,7 @@ function cartReducer(state, action) {
 }
 
 export function CartContextProvider({children}) {
-  useReducer();
+  useReducer(cartReducer, {items: []});
 
   return <CartContext.Provider>{children}</CartContext.Provider>
 }
